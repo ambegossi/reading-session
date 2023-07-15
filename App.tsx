e9d73fbef425dea754f3609207e254b7aaa620ce
Anderson Begossi <andersonbegossi@gmail.com>
2023-07-15 15:20:13 -0300

    feat: create a timer

--- App.tsx
+++ App.tsx
@@ -1,95 +1,90 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
+  TouchableOpacity,
   useColorScheme,
   View,
+  Text,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 function App(): JSX.Element {
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const [elapsedTime, setElapsedTime] = useState<number>(0);
+
+  const timerId = useRef<NodeJS.Timeout | null>(null);
+
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const startTimer = () => {
+    if (!isRunning) {
+      setIsRunning(true);
+      timerId.current = setTimeout(updateTimer, 1000);
+    }
   };
 
+  const pauseTimer = () => {
+    if (isRunning) {
+      setIsRunning(false);
+      if (timerId.current) {
+        clearTimeout(timerId.current);
+      }
+    }
+  };
+
+  const resetTimer = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+    if (timerId.current) {
+      clearTimeout(timerId.current);
+    }
+  };
+
+  const updateTimer = () => {
+    setElapsedTime(prev => prev + 1);
+    timerId.current = setTimeout(updateTimer, 1000);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timerId.current) {
+        clearTimeout(timerId.current);
+      }
+    };
+  }, []);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={styles.safeAreaView}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+        backgroundColor={isDarkMode ? Colors.darker : Colors.lighter}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      <ScrollView contentContainerStyle={styles.scrollViewContentContainer}>
+        <View style={styles.container}>
+          <View style={styles.timer}>
+            <Text style={styles.elapsedTime}>{elapsedTime}</Text>
+          </View>
+
+          {elapsedTime ? (
+            <TouchableOpacity style={styles.button} onPress={pauseTimer}>
+              <Text style={styles.buttonTitle}>Pausar sessão</Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity style={styles.button} onPress={startTimer}>
+              <Text style={styles.buttonTitle}>Iniciar sessão</Text>
+            </TouchableOpacity>
+          )}
+
+          <TouchableOpacity
+            style={StyleSheet.compose(styles.resetSessionButton, {
+              opacity: elapsedTime ? 1 : 0,
+            })}
+            onPress={resetTimer}>
+            <Text style={styles.buttonTitle}>Resetar sessão</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -97,21 +92,46 @@ function App(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: Colors.lighter,
   },
-  sectionTitle: {
+  container: {
+    backgroundColor: Colors.lighter,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scrollViewContentContainer: {
+    flex: 1,
+  },
+  timer: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 1000,
+    marginBottom: 24,
+  },
+  elapsedTime: {
     fontSize: 24,
-    fontWeight: '600',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  button: {
+    padding: 16,
+    backgroundColor: 'orange',
+    borderRadius: 12,
+    marginBottom: 8,
   },
-  highlight: {
-    fontWeight: '700',
+  buttonTitle: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  resetSessionButton: {
+    padding: 16,
+    backgroundColor: 'gray',
+    borderRadius: 12,
   },
 });
 
